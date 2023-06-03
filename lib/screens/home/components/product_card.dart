fd526467ff843643fa0808d1b61034064b79312c
vtscode <ventuscode@gmail.com>
2023-06-03 15:26:27 +0700

    update init

---
+++ lib/screens/home/components/product_card.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import '../../../components/title_text.dart';
+import '../../../models/Product.dart';
+
+import '../../../constants.dart';
+import '../../../size_config.dart';
+
+class ProductCard extends StatelessWidget {
+  const ProductCard({
+    super.key,
+    required this.product,
+    required this.press,
+  });
+
+  final Product product;
+  final Function press;
+
+  @override
+  Widget build(BuildContext context) {
+    double defaultSize = SizeConfig.defaultSize;
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        width: defaultSize * 14.5, //145
+        decoration: BoxDecoration(
+          color: kSecondaryColor,
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: AspectRatio(
+          aspectRatio: 0.693,
+          child: Column(
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 1,
+                child: Hero(
+                  tag: product.id,
+                  child: FadeInImage.assetNetwork(
+                    placeholder: "assets/spinner.gif",
+                    image: product.image,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: defaultSize),
+                child: TitleText(title: product.title),
+              ),
+              SizedBox(height: defaultSize / 2),
+              Text("\$${product.price}"),
+              Spacer(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
